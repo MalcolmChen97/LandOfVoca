62ae8e8105428e500e04507f106f360c4b23f19b
MalcolmChen97 <502829548@qq.com>
2018-05-21 15:23:24 -0700

    Add a spinner

--- MainApp/Android/app/src/main/java/com/tigerteam/landofvoca/MainPage.java
+++ MainApp/Android/app/src/main/java/com/tigerteam/landofvoca/MainPage.java
@@ -11,21 +11,34 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Book book = Book.getInstance();
+    private String bookname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        setUpspinner();
         setUpbtn();
         initilizeBook();
+    }
+
+    private void setUpspinner(){
+        Spinner bookchooser = (Spinner) findViewById(R.id.bookchooser);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.books,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bookchooser.setAdapter(adapter);
+        bookchooser.setOnItemSelectedListener(this);
     }
 
     private void setUpbtn(){
@@ -38,29 +51,29 @@ public class MainPage extends AppCompatActivity {
             }
         });
     }
-    private void initilizeBook(){
-        book.setSize(getVocaNum(this,"test"));
-        book.setEnglish(getPreferenceForVocabularyBook(this,"testeng",book.getSize()));
-        book.setChinese(getPreferenceForVocabularyBook(this,"testchi",book.getSize()));
-        Toast.makeText(this,book.getChinese().get(0)+book.getChinese().get(1)+book.getChinese().get(2)+book.getChinese().get(3),Toast.LENGTH_LONG).show();
-    }
 //    private void initilizeBook(){
-//        List<String> eng = new ArrayList<>();
-//        eng.add("apple");
-//        eng.add("banana");
-//        eng.add("nut");
-//        eng.add("coconut");
-//
-//        List<String> chi = new ArrayList<>();
-//        chi.add("苹果");
-//        chi.add("香蕉");
-//        chi.add("花生");
-//        chi.add("椰子");
-//
-//        saveVocaNum(this,"test",4);
-//        savePreferenceForVocabularyBook(this,"testeng",eng);
-//        savePreferenceForVocabularyBook(this,"testchi",chi);
+//        book.setSize(getVocaNum(this,"test"));
+//        book.setEnglish(getPreferenceForVocabularyBook(this,"testeng",book.getSize()));
+//        book.setChinese(getPreferenceForVocabularyBook(this,"testchi",book.getSize()));
+//        Toast.makeText(this,book.getChinese().get(0)+book.getChinese().get(1)+book.getChinese().get(2)+book.getChinese().get(3),Toast.LENGTH_LONG).show();
 //    }
+    private void initilizeBook(){
+        List<String> eng = new ArrayList<>();
+        eng.add("apple");
+        eng.add("banana");
+        eng.add("nut");
+        eng.add("coconut");
+
+        List<String> chi = new ArrayList<>();
+        chi.add("苹果");
+        chi.add("香蕉");
+        chi.add("花生");
+        chi.add("椰子");
+
+        saveVocaNum(this,"test",4);
+        savePreferenceForVocabularyBook(this,"testeng",eng);
+        savePreferenceForVocabularyBook(this,"testchi",chi);
+    }
 
 
     public static void saveVocaNum(Context context,String book, int size){
@@ -95,4 +108,13 @@ public class MainPage extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        bookname = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Toast.makeText(adapterView.getContext(),"you need to choose a book to start",Toast.LENGTH_SHORT);
+    }
 }
