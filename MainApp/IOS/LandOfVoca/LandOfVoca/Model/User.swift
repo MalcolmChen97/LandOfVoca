e0ebafe02e7da6362d0bce628266dcb283f0b409
MalcolmChen97 <502829548@qq.com>
2018-06-28 00:44:30 -0700

    I messed up

---
+++ MainApp/IOS/LandOfVoca/LandOfVoca/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  LandOfVoca
+//
+//  Created by ChenZengzhan on 2018-06-25.
+//  Copyright © 2018 teamtiger. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    let userName : String
+    let password : String
+    
+    init(userName : String,password : String) {
+        self.userName = userName
+        self.password = password
+    }
+    
+}
